a462ed7590d34b9604afa1c1b2c13696777e1994
Wes <wesley7879@gmail.com>
2024-02-27 21:21:26 -0500

    update setup

--- setup.py
+++ setup.py
@@ -22,26 +22,27 @@ def parse_requirements_file (filename: str) -> typing.List:
 
 
 if __name__ == "__main__":
-    base_packages = parse_requirements_file("requirements.txt")
 
     setuptools.setup(
         name="fsmirror",
-        version = 0.3,
+        version = 0.4,
         url="https://github.com/wesmadrigal/fsmirror",
         packages = setuptools.find_packages(exclude=[ "docs", "examples" ]),
-        install_requires = base_packages,
-        extras_require = {
-            "base": base_packages,
-            },
-
+        install_requires = [
+            "exceptiongroup>=1.2.0",
+            "iniconfig>=2.0.0",
+            "packaging>=23.2", 
+            "pluggy>=1.4.0",
+            "pytest>=8.0.2",
+            "PyYAML==6.0.1",
+            "tomli>=2.0.1"
+            ],
         author="Wes Madrigal",
         author_email="wes@kurve.ai",
         license="MIT",
-
         description="A metadata management package based on filesystem mirroring.",
         long_description = pathlib.Path("README.md").read_text(),
         long_description_content_type = "text/markdown",
-
         keywords = ", ".join(KEYWORDS),
         classifiers = [
             "Programming Language :: Python :: 3",
@@ -52,12 +53,10 @@ if __name__ == "__main__":
             "Intended Audience :: Education",
             "Intended Audience :: Information Technology",
             ],
-
         project_urls = {
             "Source" : "http://github.com/wesmadrigal/fsmirror",
             "Issue Tracker" : "https://github.com/wesmadrigal/fsmirror/issues"
             },
-
         include_package_data=True,
         zip_safe=False,
         )
